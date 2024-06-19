5b9b77d6e3f373b5aad4b52b9c0803e033cb6f2f
YakKyoR <cuentaJob888@gmail.com>
2024-06-19 16:03:29 -0600

    pinshi mike

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import 'react-native-gesture-handler';
 import { NavigationContainer, StackActions } from '@react-navigation/native'
 import { Text, View } from 'react-native'
 import { StackNavigation } from './presentation/routes/StackNavigation';
+import { SideMenuNavigator } from './presentation/routes/SideMenuNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-        <StackNavigation />
+        {/* <StackNavigation /> */}
+        <SideMenuNavigator/>
     </NavigationContainer>
   )
 }
--- src/presentation/screens/Home/HomeScreen.tsx
+++ src/presentation/screens/Home/HomeScreen.tsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react'
+import { type NavigationProp, useNavigation, DrawerActions } from '@react-navigation/native'
+
 import { Pressable, Text, View } from 'react-native'
 import { globalStyles } from '../../theme/theme'
-import { NavigationProp, useNavigation } from '@react-navigation/native'
 import { PrimaryButton } from '../../components/shared/PrimaryButton'
-import { RootStackParams } from '../../routes/StackNavigation'
+import type { RootStackParams } from '../../routes/StackNavigation'
+
 
 export const HomeScreen = () => {
 
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable onPress={ () => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menu</Text>
+        </Pressable>
+      )
+    })
+  }, [])
   
   return (
     <View style={globalStyles.container}>
